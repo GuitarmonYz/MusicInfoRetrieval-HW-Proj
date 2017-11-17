16806bc2ab2db277d60077efca94f32ab73f8a7e
GuitarmonYz <guitarmonyz@gmail.com>
2017-11-17 18:29:44 -0500

    HW5 bug fixing

--- HW5/assign5.m
+++ HW5/assign5.m
@@ -3,12 +3,13 @@ load('data/labels.mat');
 %% B.2
 % param init
 numClasses = 5;
+num_features = 10;
 K = 3;
 num_folds = 3;
 bestFeature = 0;
 bestAccuracy = 0;
 % test feature performance individually
-for i = 1 : numClasses
+for i = 1 : num_features
     [avg_accuracy, ~, ~] = myCrossValidation(data(i,:), labels, K, num_folds);
     if avg_accuracy > bestAccuracy
         bestAccuracy = avg_accuracy;
@@ -21,9 +22,24 @@ end
 num_folds = 10;
 K = [1,3,7];
 acc_res = zeros(1,length(K));
-confmat_res = zeros(numClass,numClasses*numClasses);
+confmat_res = zeros(numClass,numClasses*length(K));
 for i = 1 : length(K)
     [avg_accuracy, ~, tmp_confmat] = myCrossValidation(data(sel_feature_set,:), labels, K(i), num_folds);
     acc_res(i) = avg_accuracy;
     confmat_res(:,(numClasses*(i-1)+1):numClasses*i) = tmp_confmat;
 end
+%% E.2
+[c_labels, ~] = myKMeansClustering(data, 5);
+acc_array = zeros(1,5);
+kmeans_acc = zeros(1,10);
+for j = 1 : 10
+    for i = 1 : 5
+        gt_cluster = labels(labels == i);
+        est_cluster = c_labels(labels == i);
+        diff_mode = mode(gt_cluster - est_cluster);
+        acc = length(est_cluster(est_cluster == (i - diff_mode)))/length(est_cluster);
+        acc_array(i) = acc;
+    end
+    kmeans_acc(j) = mean(acc_array);
+end
+kmeans_acc = mean(kmeans_acc);
--- HW5/myKMeansClustering.m
+++ HW5/myKMeansClustering.m
@@ -10,5 +10,29 @@ function [c_labels, centroids] = myKMeansClustering(data, K)
 %   centroids:      num_features x K matrix, where each column is one of your cluster centroids
 
 %% Add your code here
-
+[num_features,num_data_points] = size(data);
+randidx = randperm(num_data_points);
+centroids = data(:,randidx(1:K));
+c_labels = (1:num_data_points);
+difference = intmax;
+while difference >= 1e-5
+    for i = 1 : num_data_points
+        tmp_point = data(:,i);
+        min_dist = intmax;
+        for j = 1 : K
+            tmp_dist = norm(tmp_point-centroids(:,j));
+            if tmp_dist < min_dist
+                min_dist = tmp_dist;
+                c_labels(i) = j;
+            end
+        end
+    end
+    new_centroids = zeros(num_features, K);
+    for i = 1 : K
+        new_centroids(:,i) = mean(data(c_labels == i),2);
+    end
+    difference = new_centroids - centroids;
+    difference = sum(difference(:));
+    centroids = new_centroids;
+end
 end
